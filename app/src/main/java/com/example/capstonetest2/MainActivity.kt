2487b76802e7e3997235f465766525442cf60c56
Sam <Sam.sallee37@gmail.com>
2023-10-11 01:40:36 -0500

    Capstone app project starting point with simple navigation bar.
    The navigation bar currently does nothing, and only contains placeholders at the moment.
    The Next step is to figure out how to create different pages and link them to the nav bar.
    
    1 error for Scaffold padding not used.(I still need to look into this, but the app runs with it so I am ignoring it for now because it's almost 2am)
    
    2 unused imports (see above)

---
+++ app/src/main/java/com/example/capstonetest2/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.example.capstonetest2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Badge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.saveable.rememberSaveable
+import  androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.capstonetest2.ui.theme.CapstoneTest2Theme
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+    //might need to add location of page for navigation
+
+)
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CapstoneTest2Theme {
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false,
+
+                        ),
+                    BottomNavigationItem(
+                        title = "PlaceHolder",
+                        selectedIcon = Icons.Filled.Star,
+                        unselectedIcon = Icons.Outlined.Star,
+                        hasNews = false,
+                        badgeCount = 69
+
+                        ),
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings,
+                        hasNews = false,
+
+                        ),
+                    //Additional navigation pages can go here
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            /*TODO navigation controller
+                                            * this is where the code will go
+                                            * that will move you to a different
+                                            * page
+                                            * */
+                                        },
+                                        label = {
+                                            Text(text = item.title)
+                                        },
+                                        icon = {
+                                            BadgedBox(
+                                                badge = {
+                                                    if(item.badgeCount != null){
+                                                        Badge {
+                                                            Text(text = item.badgeCount.toString())
+                                                        }
+                                                    }else if (item.hasNews){
+                                                        Badge()
+                                                    }
+
+                                                }
+                                            ) {
+                                                Icon(imageVector = if(index == selectedItemIndex){
+                                                    item.selectedIcon
+                                                }else item.unselectedIcon,
+                                                    contentDescription = item.title
+                                                )
+
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CapstoneTest2Theme {
+        Greeting("Android")
+    }
+}*/
